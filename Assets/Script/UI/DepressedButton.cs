9767ffbc947131fe8e2cc1cbbb304f88619608ab
Randy Knapp <randy.bravo2@gmail.com>
2018-01-21 16:25:33 -0800

    DepressedButton and a few colored buttons

---
+++ Assets/Script/UI/DepressedButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class DepressedButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
+{
+	[SerializeField]
+	private GameObject Contents;
+
+	[SerializeField]
+	private float DepressBy;
+
+	private bool _isDown;
+	private bool _isOver;
+	private Vector3 _startPos;
+
+	public void Awake()
+	{
+		_startPos = Contents.transform.localPosition;
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		_isDown = true;
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		_isDown = false;
+	}
+	
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		_isOver = true;
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		_isOver = false;
+	}
+
+	private void Update()
+	{
+		var depress = _isDown && _isOver ? DepressBy : 0;
+		Contents.transform.localPosition = _startPos + new Vector3(0, -depress, 0);
+	}
+}
